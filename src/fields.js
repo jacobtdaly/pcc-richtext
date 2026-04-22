569132c9db225448acf65c01157cb2d0660e68be
Jake Daly <47786887+jacobtdaly@users.noreply.github.com>
2026-04-22 14:16:36 -0400

    Refactor getFieldIdFromExport function

--- src/fields.js
+++ src/fields.js
@@ -1,19 +1,11 @@
-(function (window, document) {
-	'use strict';
+const RichText = window.PCC.RichText;
 
-	window.PCC = window.PCC || {};
-	window.PCC.RichText = window.PCC.RichText || {};
+RichText.getFieldIdFromExport = function (exportName) {
+	const container = document.querySelector(`div[data-export="${exportName}"]`);
 
-	const RichText = window.PCC.RichText;
+	if (!container || !container.dataset || !container.dataset.id) {
+		return null;
+	}
 
-	RichText.getFieldIdFromExport = function (exportName) {
-		const container = document.querySelector(`div[data-export="${exportName}"]`);
-
-		if (!container || !container.dataset || !container.dataset.id) {
-			return null;
-		}
-
-		return `form_${container.dataset.id}`;
-	};
-
-})(window, document);
+	return `form_${container.dataset.id}`;
+};
