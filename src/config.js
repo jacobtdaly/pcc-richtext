4dc3cd36420cdd0e1d52dedd7848de93ed770315
Jake Daly <47786887+jacobtdaly@users.noreply.github.com>
2026-04-22 14:13:03 -0400

    Refactor RichText configuration in config.js

--- src/config.js
+++ src/config.js
@@ -1,36 +1,30 @@
-(function (window) {
-	'use strict';
+window.PCC = window.PCC || {};
+window.PCC.RichText = window.PCC.RichText || {};
 
-	window.PCC = window.PCC || {};
-	window.PCC.RichText = window.PCC.RichText || {};
+const RichText = window.PCC.RichText;
 
-	const RichText = window.PCC.RichText;
+RichText.version = __VERSION__;
 
-	RichText.version = '0.1.1';
+RichText.defaults = {
+	profile: 'full',
+	height: 300,
+	fullPage: false
+};
 
-	RichText.defaults = {
-		profile: 'full',
-		height: 300,
-		fullPage: false
+RichText.getEditorConfig = function (field) {
+	const config = Object.assign({}, RichText.defaults, field);
+
+	return {
+		filebrowserImageBrowseUrl: '/fw/framework/ckfinder/ckfinder.html?type=Images&connector=' + encodeURIComponent('/www/'),
+		filebrowserBrowseUrl: '/fw/framework/ckfinder/ckfinder.html?type=Documents&connector=' + encodeURIComponent('/www/'),
+		fullPage: config.fullPage,
+		height: config.height,
+		toolbar: CKEDITOR.getToolbar(config.profile),
+
+		entities: true,
+		basicEntities: true,
+		entities_latin: true,
+		entities_greek: true,
+		entities_processNumerical: 'force'
 	};
-
-	RichText.getEditorConfig = function (field) {
-		const config = Object.assign({}, RichText.defaults, field);
-
-		return {
-			filebrowserImageBrowseUrl: '/fw/framework/ckfinder/ckfinder.html?type=Images&connector=' + encodeURIComponent('/www/'),
-			filebrowserBrowseUrl: '/fw/framework/ckfinder/ckfinder.html?type=Documents&connector=' + encodeURIComponent('/www/'),
-			fullPage: config.fullPage,
-			height: config.height,
-			toolbar: CKEDITOR.getToolbar(config.profile),
-
-			// Force numeric entities so saved HTML is safer for Slate rawhtml/XML-sensitive rendering.
-			entities: true,
-			basicEntities: true,
-			entities_latin: true,
-			entities_greek: true,
-			entities_processNumerical: 'force'
-		};
-	};
-
-})(window);
+};
