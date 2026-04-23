422719be82e8439384c82977c5f9396984d0ad42
Jake Daly <47786887+jacobtdaly@users.noreply.github.com>
2026-04-23 17:47:59 -0400

    Update file paths in build-readme script

--- scripts/build-readme.mjs
+++ scripts/build-readme.mjs
@@ -1,8 +1,8 @@
 import { readFile, writeFile } from 'node:fs/promises';
 
-const pkg = JSON.parse(await readFile(new URL('./package.json', import.meta.url), 'utf8'));
-const template = await readFile(new URL('./README.template.md', import.meta.url), 'utf8');
+const pkg = JSON.parse(await readFile(new URL('../package.json', import.meta.url), 'utf8'));
+const template = await readFile(new URL('../README.template.md', import.meta.url), 'utf8');
 
 const output = template.replace(/\{\{VERSION\}\}/g, pkg.version);
 
-await writeFile(new URL('./README.md', import.meta.url), output, 'utf8');
+await writeFile(new URL('../README.md', import.meta.url), output, 'utf8');
